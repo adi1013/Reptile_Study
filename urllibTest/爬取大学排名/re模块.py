58bcf23d9f656e1f3b3d54988d67c3d03bef67fe
934683649@qq.com <934683649@qq.com>
2018-10-15 22:35:33 +0800

    未完成

---
+++ urllibTest/爬取大学排名/re模块.py
@@ -0,0 +1,20 @@
+import re
+#第一次接触re模块
+def openFile():
+    str=''
+    with open('save.html','r',encoding='utf-8') as f:
+        str=f.read()
+        f.close()
+    return str
+
+def formatResult(str):
+    pattern=re.compile(r".*<tr(.*?)>(.*?)</tr>") #<tr.*><td.*>.*?</td></tr>
+    list=pattern.findall(str)
+
+    return list
+
+if __name__ == '__main__':
+    str=openFile()
+    list=formatResult(str)
+    for i in range(0,len(list)):
+        print(list[i])
---
+++ urllibTest/爬取大学排名/urllib简易爬取大学排名.py
@@ -0,0 +1,26 @@
+import re
+from urllib import request
+'''
+    内涵段子爬虫类
+'''
+class spider:
+
+    def load_page(self):
+        url = 'http://www.zuihaodaxue.com/zuihaodaxuepaiming2018.html'
+        headers={
+            'User_Agent':'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0'
+        }
+        req=request.Request(url=url,headers=headers)
+        response=request.urlopen(req)
+        html=response.read()
+        return html.decode()
+
+    def save_in_file(self,html):
+        with open('save.html','r+',encoding='utf-8') as f:
+            f.write(html)
+            f.close()
+
+if __name__ == '__main__':
+    spi = spider()
+    html=spi.load_page()
+    spi.save_in_file(html)
